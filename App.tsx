8bd9c9d12cfc5d85b014e5951e87d163970a6f35
koejdga <108132458+koejdga@users.noreply.github.com>
2024-04-11 13:04:12 +0300

    Add authentication through Firebase

--- App.tsx
+++ App.tsx
@@ -9,7 +9,11 @@ import Dictionary from "./app/screens/Dictionary";
 import WordTranslation from "./app/screens/WordTranslation";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Ionicons } from "@expo/vector-icons";
+import { LoadingScreen } from "./app/screens/LoadingScreen";
+import { Login } from "./app/screens/Login";
+import { Register } from "./app/screens/Register";
 
 enum Screen {
   ArticleGame,
@@ -18,6 +22,7 @@ enum Screen {
 }
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [screen, setScreen] = useState(Screen.HomePage);
@@ -31,70 +36,87 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            switch (route.name) {
-              case "Home":
-                return focused ? (
-                  <Ionicons name="home" size={size} color={color} />
-                ) : (
-                  <Ionicons name="home-outline" size={size} color={color} />
-                );
-              case "Dictionary":
-                return focused ? (
-                  <Ionicons name="bookmarks" size={size} color={color} />
-                ) : (
-                  <Ionicons
-                    name="bookmarks-outline"
-                    size={size}
-                    color={color}
-                  />
-                );
-              case "Achievements":
-                return focused ? (
-                  <Ionicons name="trophy" size={size} color={color} />
-                ) : (
-                  <Ionicons name="trophy-outline" size={size} color={color} />
-                );
-              case "Account":
-                return focused ? (
-                  <Ionicons name="person" size={size} color={color} />
-                ) : (
-                  <Ionicons name="person-outline" size={size} color={color} />
-                );
-            }
-          },
-          tabBarActiveTintColor: "black",
-          tabBarInactiveTintColor: "black",
-          tabBarShowLabel: false,
-        })}
-      >
-        <Tab.Screen name="Home" component={HomePage} />
-        <Tab.Screen
-          name="Dictionary"
-          component={Dictionary}
-          options={{ headerShown: false }}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="LoadingScreen"
+          component={LoadingScreen}
+          options={{
+            headerShown: false,
+          }}
         />
-        <Tab.Screen name="Achievements" component={Dictionary} />
-        <Tab.Screen name="Account" component={Dictionary} />
-      </Tab.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Main"
+          component={HomePage}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
-    // <View style={[styles.container, { backgroundColor: backgrounds[screen] }]}>
-    //   <SafeAreaView style={[styles.container]}>
-    //     <View style={styles.container}>
-    //       {screen == Screen.ArticleGame && <ArticleGame />}
-    //       {screen == Screen.HomePage && (
-    //         <HomePage onGameStart={handleGameStart} />
-    //       )}
-    //       {screen == Screen.Dictionary && <WordTranslation />}
-    //     </View>
-    //     <Navigation
-    //       onGoToHomePage={handleGoToHomePage}
-    //       onGoToDictPage={handleGoToDictPage}
+    // <NavigationContainer>
+    //   <Tab.Navigator
+    //     screenOptions={({ route }) => ({
+    //       tabBarIcon: ({ focused, color, size }) => {
+    //         switch (route.name) {
+    //           case "Home":
+    //             return focused ? (
+    //               <Ionicons name="home" size={size} color={color} />
+    //             ) : (
+    //               <Ionicons name="home-outline" size={size} color={color} />
+    //             );
+    //           case "Dictionary":
+    //             return focused ? (
+    //               <Ionicons name="bookmarks" size={size} color={color} />
+    //             ) : (
+    //               <Ionicons
+    //                 name="bookmarks-outline"
+    //                 size={size}
+    //                 color={color}
+    //               />
+    //             );
+    //           case "Achievements":
+    //             return focused ? (
+    //               <Ionicons name="trophy" size={size} color={color} />
+    //             ) : (
+    //               <Ionicons name="trophy-outline" size={size} color={color} />
+    //             );
+    //           case "Account":
+    //             return focused ? (
+    //               <Ionicons name="person" size={size} color={color} />
+    //             ) : (
+    //               <Ionicons name="person-outline" size={size} color={color} />
+    //             );
+    //         }
+    //       },
+    //       tabBarActiveTintColor: "black",
+    //       tabBarInactiveTintColor: "black",
+    //       tabBarShowLabel: false,
+    //     })}
+    //   >
+    //     <Tab.Screen name="Home" component={HomePage} />
+    //     <Tab.Screen
+    //       name="Dictionary"
+    //       component={Dictionary}
+    //       options={{ headerShown: false }}
     //     />
-    //   </SafeAreaView>
-    // </View>
+    //     <Tab.Screen name="Achievements" component={Dictionary} />
+    //     <Tab.Screen name="Account" component={Dictionary} />
+    //   </Tab.Navigator>
+    // </NavigationContainer>
   );
 }
 
